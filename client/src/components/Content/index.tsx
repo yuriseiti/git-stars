35786ad32b48f2ce3a081cd0ba01e756bd16f980
Yuri Seiti <yuriseiti@gmail.com>
2024-10-24 21:46:56 -0400

    remove unused import

--- client/src/components/Content/index.tsx
+++ client/src/components/Content/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import { Container } from "./styles";
 import InfoCard from "./components/InfoCard";
 import RankingCard from "./components/RankingCard";
